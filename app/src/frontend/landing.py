14ffb6663ddc0cabcd55a6978d241cea097f164b
Pavel Dokuchaev <mrpauldok@gmail.com>
2025-03-24 19:01:59 +0300

    Interactive experiments (except charts)

--- app/src/frontend/landing.py
+++ app/src/frontend/landing.py
@@ -4,6 +4,11 @@ import sys
 
 import streamlit as st
 from src.core import utils
+from src.strategy.bb_strategy import BollingerBandsStrategy
+from src.strategy.macd_strategy import MACDStrategy
+from src.strategy.sma_cross_strategy import SmaCross
+from src.strategy.stoch_strategy import STOCHStrategy
+from src.strategy.tema_strategy import TEMAStrategy
 
 logger = logging.getLogger()
 
@@ -18,12 +23,107 @@ def data_update_callback() -> None:
     )
 
 
+def backtest_and_tune_callback() -> None:
+    # Get history from cache and split it
+    ticker_data = utils.get_history(
+        tickers=st.session_state["backtest_ticker_sb"],
+        start=st.session_state["backtest_train_start_dt"].strftime("%Y-%m-%d"),
+        end=st.session_state["backtest_validation_end_dt"].strftime("%Y-%m-%d"),
+        interval=st.session_state["backtest_interval_sb"],
+        update_cache=False,
+    )
+    X_train, y_train, X_test, y_test, X_val, y_val = utils.train_test_valid_split(
+        ticker_data,
+        train_start=st.session_state["backtest_train_start_dt"].strftime("%Y-%m-%d"),
+        train_end=st.session_state["backtest_train_end_dt"].strftime("%Y-%m-%d"),
+        test_end=st.session_state["backtest_test_end_dt"].strftime("%Y-%m-%d"),
+        valid_end=st.session_state["backtest_validation_end_dt"].strftime("%Y-%m-%d"),
+        drop_leaky=False,
+    )
+
+    # Perform selection of best hyperparameters
+    full_strategy_test_list = [
+        {
+            "strategy_type": "SmaCross",
+            "strategy_class": SmaCross,
+            "strategy_params_options": {
+                "ma_fast_periods": [2, 3, 5, 7, 10],
+                "ma_slow_periods": [5, 7, 10, 14, 20, 30],
+            },
+        },
+        {
+            "strategy_type": "MACDStrategy",
+            "strategy_class": MACDStrategy,
+            "strategy_params_options": {
+                "fastperiod": [3, 5, 7, 14, 20],
+                "slowperiod": [14, 20, 26, 30, 40],
+                "signalperiod": [7, 9, 11, 14],
+            },
+        },
+        {
+            "strategy_type": "STOCHStrategy",
+            "strategy_class": STOCHStrategy,
+            "strategy_params_options": {
+                "fastk_period": [7, 10, 14, 20],
+                "slowk_period": [5, 7, 10],
+                "slowk_matype": [0],
+                "slowd_period": [3, 7, 10],
+                "slowd_matype": [0],
+            },
+        },
+        {
+            "strategy_type": "TEMAStrategy",
+            "strategy_class": TEMAStrategy,
+            "strategy_params_options": {
+                "period": [14, 21, 28, 40, 55, 70, 90],
+            },
+        },
+        {
+            "strategy_type": "BollingerBandsStrategy",
+            "strategy_class": BollingerBandsStrategy,
+            "strategy_params_options": {
+                "period": [14, 21, 28, 40, 55, 70, 90],
+            },
+        },
+    ]
+    best_strategy_class, best_params, best_performance, test_summary = (
+        utils.get_best_strategy(
+            full_strategy_test_list,
+            y_test=y_test,
+            X_test=X_test,
+            kpi=st.session_state["backtest_kpi_sb"],
+        )
+    )
+
+    # Validate results on Validation dataset
+    validation_summary = utils.validate_model_performances(
+        y_val=y_val,
+        X_val=X_val,
+        full_test_summary=test_summary,
+        kpi=st.session_state["backtest_kpi_sb"],
+    )
+
+    # Save result to session_state
+    overall_result = {}
+    for strategy_type in test_summary.keys():
+        overall_result[strategy_type] = {
+            "params": test_summary[strategy_type]["params"],
+            "performance_test": test_summary[strategy_type]["performance"],
+            "performance_val": validation_summary[strategy_type]["val_performance"],
+            "fig_test": test_summary[strategy_type]["test_fig"],
+            "fig_val": validation_summary[strategy_type]["val_figure"],
+        }
+    st.session_state["strategy_backtesting_result"] = overall_result
+
+
 # ~ ~ ~ UI ~ ~ ~
 # Make sure to use full screen width
 st.set_page_config(layout="wide")
 
 st.title("ML Finance homework demo page")
 
+# = = = = = = = = = = = = = = = = = = = = =
+st.header("Download to local cache section")
 # Selectors to update local data
 with st.expander(
     label="Update local data cache controls",
@@ -57,7 +157,8 @@ with st.expander(
         on_click=data_update_callback,
     )
 
-
+# = = = = = = = = = = = = = = = = = = = = =
+st.header("History visualization")
 # Visualization controls
 with st.container(border=True):
     # Filters
@@ -159,3 +260,73 @@ with st.container(border=True):
                 expanded=True,
             ):
                 st.plotly_chart(charts["stochastic"])
+
+
+# = = = = = = = = = = = = = = = = = = = = =
+st.header("Strategy Backtesting")
+with st.container(border=True):
+    st.write("**Please update local cache before running experiment**")
+
+    # Container with controls
+    with st.container(border=True):
+        # Filters
+        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        with col1:
+            st.selectbox(label="Interval", options=["1d"], key="backtest_interval_sb")
+        with col2:
+            st.selectbox(
+                label="Ticker",
+                options=utils.CACHED_CONFIG.TICKERS,
+                index=0,  # 0: "BTC-USD", 1: "ETH-USD"
+                key="backtest_ticker_sb",
+            )
+        with col3:
+            st.date_input(
+                label="Train start", key="backtest_train_start_dt", value="2010-01-01"
+            )
+        with col4:
+            st.date_input(
+                label="Train end", key="backtest_train_end_dt", value="2023-01-01"
+            )
+        with col5:
+            st.date_input(
+                label="Test end", key="backtest_test_end_dt", value="2024-01-01"
+            )
+        with col6:
+            st.date_input(
+                label="Validation end",
+                key="backtest_validation_end_dt",
+                value="2025-01-01",
+            )
+        with col7:
+            st.selectbox(label="KPI", options=["Return [%]"], key="backtest_kpi_sb")
+
+        # Button to run model parameter tuning on Test dataset, and validate results on Validation dataset
+        st.button(
+            label="Run parameter tuning for different Strategies",
+            use_container_width=True,
+            on_click=backtest_and_tune_callback,
+        )
+
+    # Container with charts
+    with st.container(border=True):
+        if "strategy_backtesting_result" not in st.session_state:
+            st.write("**Run a test to see result visualizations**")
+        else:
+            for strategy_type, result in st.session_state[
+                "strategy_backtesting_result"
+            ].items():
+                with st.container(border=True):
+                    st.subheader(strategy_type)
+                    st.write(f"**Tuned parameters: {result['params']}**")
+                    col_test, col_val = st.columns(2)
+                    with col_test:
+                        st.write(
+                            f"**TEST {st.session_state['backtest_kpi_sb']}: {result['performance_test']}**"
+                        )
+                        # st.bokeh_chart(result["fig_test"])
+                    with col_val:
+                        st.write(
+                            f"**VALIDATION {st.session_state['backtest_kpi_sb']}: {result['performance_val']}**"
+                        )
+                        # st.bokeh_chart(result["fig_val"])
