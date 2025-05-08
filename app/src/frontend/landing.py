f2526b1e9f05b7e67b6045f0a98ba7d326ce797f
Pavel Dokuchaev <mrpauldok@gmail.com>
2025-05-08 21:03:34 +0300

    Streamlit page refactored to tabs

--- app/src/frontend/landing.py
+++ app/src/frontend/landing.py
@@ -123,219 +123,249 @@ st.set_page_config(layout="wide")
 
 st.title("ML Finance homework demo page")
 
-# = = = = = = = = = = = = = = = = = = = = =
-st.header("Download to local cache section")
-# Selectors to update local data
-with st.expander(
-    label="Update local data cache controls",
-    expanded=False,
-    icon=":material/change_circle:",
-):
-    # st.header("Update local data cache controls")
+# Organize logical blocks as tabs
+tab_download, tab_history_chart, tab_backtesting = st.tabs(
+    ["Download to cache", "📈 History visualization", "Strategy backtesting"]
+)
 
-    # Filters
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.selectbox(label="Interval", options=["1d"], key="update_interval_sb")
-    with col2:
-        st.multiselect(
-            label="Tickers",
-            options=utils.CACHED_CONFIG.TICKERS,  # ["BTC-USD", "A"],  # TODO: change to depend on config
-            default=utils.CACHED_CONFIG.TICKERS,  # ["BTC-USD", "A"],  # TODO: change to depend on config
-            key="update_tickers_ms",
-        )
-    with col3:
-        st.date_input(
-            label="From", key="update_start_dt", value=utils.CACHED_CONFIG.START_DT
-        )
-    with col4:
-        st.date_input(label="To", key="update_end_dt", value=utils.CACHED_CONFIG.END_DT)
-
-    # Button to run refresh
-    st.button(
-        label="Update data in local cache",
-        use_container_width=True,
-        on_click=data_update_callback,
-    )
-
-# = = = = = = = = = = = = = = = = = = = = =
-st.header("History visualization")
-# Visualization controls
-with st.container(border=True):
-    # Filters
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.selectbox(label="Interval", options=["1d"], key="show_interval_sb")
-    with col2:
-        st.multiselect(
-            label="Tickers",
-            options=utils.CACHED_CONFIG.TICKERS,
-            default=["BTC-USD"],
-            key="show_tickers_ms",
-        )
-    with col3:
-        st.date_input(
-            label="From", key="show_start_dt", value=utils.CACHED_CONFIG.START_DT
-        )
-    with col4:
-        st.date_input(label="To", key="show_end_dt", value=utils.CACHED_CONFIG.END_DT)
-
-    # Bool options
+with tab_download:
+    # = = = = = = = = = = = = = = = = = = = = =
+    st.header("Download to local cache section")
+    # Selectors to update local data
     with st.expander(
-        label="Chart options",
+        label="Update local data cache controls",
         expanded=False,
-        icon=":material/settings:",
+        icon=":material/change_circle:",
     ):
-        col1, col2, col3, col4, col5, col6 = st.columns(6)
-        with col1:
-            st.checkbox(label="Update cache", value=False, key="options_update_cb")
-        with col2:
-            st.checkbox(label="Draw Close", value=True, key="options_draw_close_cb")
-        with col3:
-            st.checkbox(label="Draw Volume", value=True, key="options_draw_volume_cb")
-        with col4:
-            st.checkbox(label="Scale Price", value=False, key="options_scale_price_cb")
-        with col5:
-            st.checkbox(label="Draw SMA and EMA", value=True, key="options_draw_ma_cb")
-        with col6:
-            st.slider("MA smoothing period", 0, 30, 3, 1, key="options_draw_ma_slider")
+        # st.header("Update local data cache controls")
 
+        # Filters
         col1, col2, col3, col4 = st.columns(4)
         with col1:
-            st.checkbox(
-                label="Draw Waterfall chart", value=True, key="options_draw_waterfall"
-            )
+            st.selectbox(label="Interval", options=["1d"], key="update_interval_sb")
         with col2:
-            st.checkbox(
-                label="Draw Stochastic Oscillator chart",
-                value=True,
-                key="options_draw_stochastic",
+            st.multiselect(
+                label="Tickers",
+                options=utils.CACHED_CONFIG.TICKERS,  # ["BTC-USD", "A"],  # TODO: change to depend on config
+                default=utils.CACHED_CONFIG.TICKERS,  # ["BTC-USD", "A"],  # TODO: change to depend on config
+                key="update_tickers_ms",
             )
         with col3:
-            st.slider(
-                "Fast_K Stochastic period", 0, 30, 14, 1, key="options_fast_k_slider"
+            st.date_input(
+                label="From", key="update_start_dt", value=utils.CACHED_CONFIG.START_DT
             )
         with col4:
-            st.slider(
-                "Slow_D Stochastic period", 0, 30, 3, 1, key="options_slow_d_slider"
+            st.date_input(
+                label="To", key="update_end_dt", value=utils.CACHED_CONFIG.END_DT
             )
 
-    # Display chart
-    if len(st.session_state["show_tickers_ms"]) > 0:
-        charts = utils.get_data_and_draw_figure(
-            tickers=st.session_state["show_tickers_ms"],
-            start=st.session_state["show_start_dt"].strftime("%Y-%m-%d"),
-            end=st.session_state["show_end_dt"].strftime("%Y-%m-%d"),
-            interval=st.session_state["show_interval_sb"],
-            update_cache=st.session_state["options_update_cb"],
-            draw_close=st.session_state["options_draw_close_cb"],
-            draw_volume=st.session_state["options_draw_volume_cb"],
-            scale_price=st.session_state["options_scale_price_cb"],
-            draw_ma=st.session_state["options_draw_ma_cb"],
-            ma_smooth_periods=st.session_state["options_draw_ma_slider"],
-            draw_waterfall=st.session_state["options_draw_waterfall"],
-            draw_stochastic=st.session_state["options_draw_stochastic"],
-            fastk_period=st.session_state["options_fast_k_slider"],
-            slowd_period=st.session_state["options_slow_d_slider"],
+        # Button to run refresh
+        st.button(
+            label="Update data in local cache",
+            use_container_width=True,
+            on_click=data_update_callback,
         )
 
-        # Draw main chart
-        with st.expander(
-            label="Price and Volume chart",
-            expanded=True,
-        ):
-            st.plotly_chart(charts["main"])
-
-        # Draw Waterfall
-        if st.session_state["options_draw_waterfall"]:
-            with st.expander(
-                label="Waterfall chart",
-                expanded=True,
-            ):
-                st.plotly_chart(charts["waterfall"])
-
-        # Draw Stochastic
-        if st.session_state["options_draw_stochastic"]:
-            with st.expander(
-                label="Stochastic oscillator chart",
-                expanded=True,
-            ):
-                st.plotly_chart(charts["stochastic"])
-
-
-# = = = = = = = = = = = = = = = = = = = = =
-st.header("Strategy Backtesting")
-with st.container(border=True):
-    st.write("**Please update local cache before running experiment**")
-
-    # Container with controls
+with tab_history_chart:
+    # = = = = = = = = = = = = = = = = = = = = =
+    st.header("History visualization")
+    # Visualization controls
     with st.container(border=True):
         # Filters
-        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        col1, col2, col3, col4 = st.columns(4)
         with col1:
-            st.selectbox(label="Interval", options=["1d"], key="backtest_interval_sb")
+            st.selectbox(label="Interval", options=["1d"], key="show_interval_sb")
         with col2:
-            st.selectbox(
-                label="Ticker",
+            st.multiselect(
+                label="Tickers",
                 options=utils.CACHED_CONFIG.TICKERS,
-                index=0,  # 0: "BTC-USD", 1: "ETH-USD"
-                key="backtest_ticker_sb",
+                default=["BTC-USD"],
+                key="show_tickers_ms",
             )
         with col3:
             st.date_input(
-                label="Train start", key="backtest_train_start_dt", value="2010-01-01"
+                label="From", key="show_start_dt", value=utils.CACHED_CONFIG.START_DT
             )
         with col4:
             st.date_input(
-                label="Train end", key="backtest_train_end_dt", value="2023-01-01"
+                label="To", key="show_end_dt", value=utils.CACHED_CONFIG.END_DT
             )
-        with col5:
-            st.date_input(
-                label="Test end", key="backtest_test_end_dt", value="2024-01-01"
-            )
-        with col6:
-            st.date_input(
-                label="Validation end",
-                key="backtest_validation_end_dt",
-                value="2025-01-01",
-            )
-        with col7:
-            st.selectbox(label="KPI", options=["Return [%]"], key="backtest_kpi_sb")
 
-        # Button to run model parameter tuning on Test dataset, and validate results on Validation dataset
-        st.button(
-            label="Run parameter tuning for different Strategies",
-            use_container_width=True,
-            on_click=backtest_and_tune_callback,
-        )
+        # Bool options
+        with st.expander(
+            label="Chart options",
+            expanded=False,
+            icon=":material/settings:",
+        ):
+            col1, col2, col3, col4, col5, col6 = st.columns(6)
+            with col1:
+                st.checkbox(label="Update cache", value=False, key="options_update_cb")
+            with col2:
+                st.checkbox(label="Draw Close", value=True, key="options_draw_close_cb")
+            with col3:
+                st.checkbox(
+                    label="Draw Volume", value=True, key="options_draw_volume_cb"
+                )
+            with col4:
+                st.checkbox(
+                    label="Scale Price", value=False, key="options_scale_price_cb"
+                )
+            with col5:
+                st.checkbox(
+                    label="Draw SMA and EMA", value=True, key="options_draw_ma_cb"
+                )
+            with col6:
+                st.slider(
+                    "MA smoothing period", 0, 30, 3, 1, key="options_draw_ma_slider"
+                )
 
-    # Container with charts
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                st.checkbox(
+                    label="Draw Waterfall chart",
+                    value=True,
+                    key="options_draw_waterfall",
+                )
+            with col2:
+                st.checkbox(
+                    label="Draw Stochastic Oscillator chart",
+                    value=True,
+                    key="options_draw_stochastic",
+                )
+            with col3:
+                st.slider(
+                    "Fast_K Stochastic period",
+                    0,
+                    30,
+                    14,
+                    1,
+                    key="options_fast_k_slider",
+                )
+            with col4:
+                st.slider(
+                    "Slow_D Stochastic period", 0, 30, 3, 1, key="options_slow_d_slider"
+                )
+
+        # Display chart
+        if len(st.session_state["show_tickers_ms"]) > 0:
+            charts = utils.get_data_and_draw_figure(
+                tickers=st.session_state["show_tickers_ms"],
+                start=st.session_state["show_start_dt"].strftime("%Y-%m-%d"),
+                end=st.session_state["show_end_dt"].strftime("%Y-%m-%d"),
+                interval=st.session_state["show_interval_sb"],
+                update_cache=st.session_state["options_update_cb"],
+                draw_close=st.session_state["options_draw_close_cb"],
+                draw_volume=st.session_state["options_draw_volume_cb"],
+                scale_price=st.session_state["options_scale_price_cb"],
+                draw_ma=st.session_state["options_draw_ma_cb"],
+                ma_smooth_periods=st.session_state["options_draw_ma_slider"],
+                draw_waterfall=st.session_state["options_draw_waterfall"],
+                draw_stochastic=st.session_state["options_draw_stochastic"],
+                fastk_period=st.session_state["options_fast_k_slider"],
+                slowd_period=st.session_state["options_slow_d_slider"],
+            )
+
+            # Draw main chart
+            with st.expander(
+                label="Price and Volume chart",
+                expanded=True,
+            ):
+                st.plotly_chart(charts["main"])
+
+            # Draw Waterfall
+            if st.session_state["options_draw_waterfall"]:
+                with st.expander(
+                    label="Waterfall chart",
+                    expanded=True,
+                ):
+                    st.plotly_chart(charts["waterfall"])
+
+            # Draw Stochastic
+            if st.session_state["options_draw_stochastic"]:
+                with st.expander(
+                    label="Stochastic oscillator chart",
+                    expanded=True,
+                ):
+                    st.plotly_chart(charts["stochastic"])
+
+with tab_backtesting:
+    # = = = = = = = = = = = = = = = = = = = = =
+    st.header("Strategy Backtesting")
     with st.container(border=True):
-        if "strategy_backtesting_result" not in st.session_state:
-            st.write("**Run a test to see result visualizations**")
-        else:
-            for strategy_type, result in st.session_state[
-                "strategy_backtesting_result"
-            ].items():
-                with st.container(border=True):
-                    st.subheader(strategy_type)
-                    st.write(f"**Tuned parameters: {result['params']}**")
-                    col_test, col_val = st.columns(2)
-                    with col_test:
-                        st.write(
-                            f"**TEST {st.session_state['backtest_kpi_sb']}: {result['performance_test']}**"
-                        )
-                        streamlit_bokeh(
-                            result["fig_test"],
-                            use_container_width=True,
-                            key=f"{strategy_type}_test_fig",
-                        )
-                    with col_val:
-                        st.write(
-                            f"**VALIDATION {st.session_state['backtest_kpi_sb']}: {result['performance_val']}**"
-                        )
-                        streamlit_bokeh(
-                            result["fig_val"],
-                            use_container_width=True,
-                            key=f"{strategy_type}_val_fig",
-                        )
+        st.write("**Please update local cache before running experiment**")
+
+        # Container with controls
+        with st.container(border=True):
+            # Filters
+            col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+            with col1:
+                st.selectbox(
+                    label="Interval", options=["1d"], key="backtest_interval_sb"
+                )
+            with col2:
+                st.selectbox(
+                    label="Ticker",
+                    options=utils.CACHED_CONFIG.TICKERS,
+                    index=0,  # 0: "BTC-USD", 1: "ETH-USD"
+                    key="backtest_ticker_sb",
+                )
+            with col3:
+                st.date_input(
+                    label="Train start",
+                    key="backtest_train_start_dt",
+                    value="2010-01-01",
+                )
+            with col4:
+                st.date_input(
+                    label="Train end", key="backtest_train_end_dt", value="2023-01-01"
+                )
+            with col5:
+                st.date_input(
+                    label="Test end", key="backtest_test_end_dt", value="2024-01-01"
+                )
+            with col6:
+                st.date_input(
+                    label="Validation end",
+                    key="backtest_validation_end_dt",
+                    value="2025-01-01",
+                )
+            with col7:
+                st.selectbox(label="KPI", options=["Return [%]"], key="backtest_kpi_sb")
+
+            # Button to run model parameter tuning on Test dataset, and validate results on Validation dataset
+            st.button(
+                label="Run parameter tuning for different Strategies",
+                use_container_width=True,
+                on_click=backtest_and_tune_callback,
+            )
+
+        # Container with charts
+        with st.container(border=True):
+            if "strategy_backtesting_result" not in st.session_state:
+                st.write("**Run a test to see result visualizations**")
+            else:
+                for strategy_type, result in st.session_state[
+                    "strategy_backtesting_result"
+                ].items():
+                    with st.container(border=True):
+                        st.subheader(strategy_type)
+                        st.write(f"**Tuned parameters: {result['params']}**")
+                        col_test, col_val = st.columns(2)
+                        with col_test:
+                            st.write(
+                                f"**TEST {st.session_state['backtest_kpi_sb']}: {result['performance_test']}**"
+                            )
+                            streamlit_bokeh(
+                                result["fig_test"],
+                                use_container_width=True,
+                                key=f"{strategy_type}_test_fig",
+                            )
+                        with col_val:
+                            st.write(
+                                f"**VALIDATION {st.session_state['backtest_kpi_sb']}: {result['performance_val']}**"
+                            )
+                            streamlit_bokeh(
+                                result["fig_val"],
+                                use_container_width=True,
+                                key=f"{strategy_type}_val_fig",
+                            )
